3fc3a6e33676d2364176a5e8bc14a56be355da2f
Sergio <maartyx96@gmail.com>
2018-05-04 17:57:42 +0200

    WebView
    Webview añadido.

--- ProyectoFinal/app/src/main/java/org/insbaixcamp/proyectofinal/NewsActivity.java
+++ ProyectoFinal/app/src/main/java/org/insbaixcamp/proyectofinal/NewsActivity.java
@@ -2,12 +2,18 @@ package org.insbaixcamp.proyectofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class NewsActivity extends AppCompatActivity {
+    WebView wvNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        wvNews=findViewById(R.id.wvNews);
+        wvNews.loadUrl("http://dcshoes.es/skate/news/");
     }
 }
+//http://www.go-skateboarding.com/?cat=4
